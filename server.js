41a9d48c2df7d0849e0b3eae3413bfe451165130
Dev Guy <ITs@Private>
2017-11-04 21:14:19 +0200

    Added script that reads NFC input and passes that to a NodeJS server

---
+++ server.js
@@ -0,0 +1,18 @@
+var http = require("http");
+var server = http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write('<!DOCTYPE "html">');
+  response.write("<html>");
+  response.write("<head>");
+  response.write("<title>Hello World Page</title>");
+  response.write("</head>");
+  response.write("<body>");
+  response.write("Hello World!");
+  response.write("</body>");
+  response.write("</html>");
+  response.end();
+  console.log(request.url);
+});
+
+server.listen(8080);
+console.log("Server is listening");
