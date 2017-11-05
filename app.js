f087becb57be2f5b32841416fac3ce1d7e668f7f
florin.scripcaru <florin.scripcaru@accenture.com>
2017-11-05 14:58:10 +0200

    Set Headers

--- app.js
+++ app.js
@@ -3,6 +3,18 @@ var app = express();
 var mysql = require('mysql');
 var WebSocket = require('ws');
 
+// Add headers
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+
+    next();
+});
+
+
 // set Web Socket
 // var ws = new WebSocket('ws://www.host.com/path', {
 // 	perMessageDeflate: false
@@ -117,6 +129,7 @@ app.use('/employees', employees);
 app.use('/assets', express.static(__dirname + '/assets'));
 app.use('/scripts', express.static(__dirname + '/scripts'));
 app.use('/tmp', express.static(__dirname + '/tmp'));
+
 
 app.listen(3000, function(){
 	console.log('Server running at port 3000: http://127.0.0.1:3000');
